83a12f3f73cc8be9a0f7c1c007029261be85d9c8
Ssitrof <fernando_ff1@tesch.edu.mx>
2026-01-21 20:24:42 -0600

    fix: use bcryptjs instead of bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const Usuario = require("../models/Usuario");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const enviarCorreo = require("../utils/enviarCorreo");
